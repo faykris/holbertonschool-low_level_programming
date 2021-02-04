f8e5a3cdf8f8c180802c720a0de1c9aaf59ce294
Cristian Pinzón <faykris28@gmail.com>
2021-02-04 14:19:59 -0500

    update last_digit file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,12 +10,12 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n, d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int d = n % 10;
+	d = n % 10;
 
 	if (d > 5)
 	{
