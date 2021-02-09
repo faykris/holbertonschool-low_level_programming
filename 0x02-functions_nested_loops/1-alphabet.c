bd78d445dd1f656722042a311e1c104a7ab9ab6f
Cristian Pinzón <faykris28@gmail.com>
2021-02-09 09:33:03 -0500

    add alphabet file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - Write alphabet in lowercase
+ * Description - print alphabet with _putchar
+ *
+ * Return: On success - 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
