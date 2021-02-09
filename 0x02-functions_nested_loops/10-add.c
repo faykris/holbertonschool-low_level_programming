202ddecc0f86c8a47d81bb2a23a003cbb9f4ee48
Cristian Pinzón <faykris28@gmail.com>
2021-02-09 14:27:53 -0500

    add add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - returns the sum of two integers
+ * @x: Is an Integer
+ * @y: Is an Integer
+ * Return: the result of the operation
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
