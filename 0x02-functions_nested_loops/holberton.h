24b5cdb737e8e26a51c8cfb734eb348aded334dd
Cristian Pinzón <faykris28@gmail.com>
2021-02-09 09:24:39 -0500

    add holberton.h file

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,43 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/* prints a char to the stdout */
+int _putchar(char c);
+
+/*prints the alphabet, in lowercase*/
+void print_alphabet(void);
+
+/*prints the alphabet in ten different lines*/
+void print_alphabet_x10(void);
+
+/*comment*/
+int _islower(int c);
+
+/*comment*/
+int _isalpha(int c);
+
+/*comment*/
+int print_sign(int n);
+
+/*comment*/
+int _abs(int);
+
+/*comment*/
+int print_last_digit(int);
+
+/*comment*/
+void jack_bauer(void);
+
+/*comment*/
+void times_table(void);
+
+/*comment*/
+int add(int, int);
+
+/*comment*/
+void print_to_98(int n);
+
+/*comment*/
+void print_times_table(int n);
+
+#endif /* holberton.h */
