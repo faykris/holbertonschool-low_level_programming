f6c8a2c219d69b2746cd97c7b14fb5340b781328
Cristian Pinzón <faykris28@gmail.com>
2021-02-18 16:39:55 -0500

    update 2-strncpy.c file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,6 +8,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i = 0;
 
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}    
+	return (dest);
 }
-
