56b388523f25ad603925324ab190acbae5c9deaf
Cristian Pinzón <faykris28@gmail.com>
2021-02-18 23:42:53 -0500

    update 8-rot13.c file

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,10 +1,21 @@
 #include "holberton.h"
 /**
  * rot13 - function that encodes a string using rot13.
- * @a: pointer
+ * @s: pointer
  * Return: char value
  */
-char *rot13(char *)
+char *rot13(char *s)
 {
+	int i = 0;
 
+	while (s[i])
+	{
+
+		if ((s[i] >= 65 && s[i] <= 77) || (s[i] >= 97 && s[i] <= 109))
+			s[i] += 13;
+		else if ((s[i] >= 78 && s[i] <= 90) || (s[i] >= 110 && s[i] <= 122))
+			s[i] -= 13;
+		i++;
+	}
+	return (s);
 }
