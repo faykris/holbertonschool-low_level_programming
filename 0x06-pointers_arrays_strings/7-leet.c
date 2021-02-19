42bbf75e205fd0125e10de559b68317f6ae248a0
Cristian Pinzón <faykris28@gmail.com>
2021-02-18 22:05:32 -0500

    update 7-leet.c file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,28 @@
 #include "holberton.h"
 /**
  * leet - function that encodes a string into 1337
- * @a: pointer
+ * @s: pointer
  * Return: char value
  */
-char *leet(char *)
+char *leet(char *s)
 {
+	int i = 0, j = 0;
+	char *let = "AaEeOoTtLl";
+	char *num = "4433007711";
 
+	while (s[i])
+	{
+		while (let[j])
+		{
+			if (s[i] == let[j])
+			{
+				s[i] = num[j];
+				break;
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (s);
 }
