505100c4618fd801e0791ea0ae62f50c5a5116df
Cristian Pinzón <faykris28@gmail.com>
2021-02-22 22:42:24 -0500

    update 7-print_chessboard.c file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,5 +6,17 @@
  */
 void print_chessboard(char (*a)[8])
 {
+	unsigned int row = 0, col = 0;
 
+	while (row < sizeof(a))
+	{
+		while (col < sizeof(a))
+		{
+			_putchar(a[row][col]);
+			col++;
+		}
+		_putchar('\n');
+		col = 0;
+		row++;
+	}
 }
