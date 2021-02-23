0432877d030b1f3bd130bea22483573ae7fbfc94
Cristian Pinzón <faykris28@gmail.com>
2021-02-22 21:40:07 -0500

    update 5-strstr.c file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * _strstr - function that locates a substring
  * @haystack: string pointer
@@ -7,5 +8,31 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i = 0, j = 0, len = 0, loc = 0;
 
+	while (needle[len])
+	{
+		len++;
+	}
+	while (haystack[i])
+	{
+		if (haystack[i] == needle[0])
+		{
+			while (haystack[i + j] == needle[j])
+			{
+				loc++;
+				if (loc == len)
+				{
+					return (haystack + i);
+				}
+				j++;
+			}
+			j = 0;
+			loc = 0;
+		}
+		else if (len == 0)
+			return (haystack);
+		i++;
+	}
+	return (NULL);
 }
