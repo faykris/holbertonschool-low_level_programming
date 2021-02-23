40c54e17685971dbecbbe4f518d3e11c3205d2b4
Cristian Pinzón <faykris28@gmail.com>
2021-02-22 21:13:39 -0500

    update 4-strpbrk.c file

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * _strpbrk - function that searches a string for any of a set of bytes
  * @s: string pointer
@@ -7,5 +8,20 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
+	unsigned int i = 0, j = 0;
 
+	while (s[i])
+	{
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (NULL);
 }
