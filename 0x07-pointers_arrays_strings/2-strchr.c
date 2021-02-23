c143a5cd8982f9dbc0aec43bf041c039bb6eec5a
Cristian Pinzón <faykris28@gmail.com>
2021-02-22 19:18:54 -0500

    update 2-strchr.c file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * _strchr - function that locates a character in a string
  * @s: string pointer
@@ -7,5 +8,20 @@
  */
 char *_strchr(char *s, char c)
 {
+	int i = 0, len = 0;
 
-}
+	while (s[len])
+	{
+		len++;
+	}
+	while (i <= len)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+		return (NULL);
+	}
+
