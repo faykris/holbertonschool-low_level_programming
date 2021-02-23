45df2baf3b6b066970ccf3c100c08d54188ac899
Cristian Pinzón <faykris28@gmail.com>
2021-02-22 20:07:24 -0500

    update 3-strspn.c file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,5 +7,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int i = 0, j = 0, u = 0, ter = 0;
 
+	while (s[i])
+	{
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				ter = 0;
+				u++;
+				break;
+			}
+			else
+				ter = 1;
+			j++;
+		}
+		if (ter == 1)
+			break;
+		j = 0;
+		i++;
+	}
+	return (u);
 }
