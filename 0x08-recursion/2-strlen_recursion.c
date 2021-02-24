3286f6be027358e6a592284dcd90b9bc0a4fb253
Cristian Pinzón <faykris28@gmail.com>
2021-02-24 18:38:18 -0500

    update 2-strlen_recursion.c without int variable and only return

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,16 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
 	if (*s != '\0')
 	{
-		s++;
-		_strlen_recursion(s);
-		len = (_strlen_recursion(s) + 1);
-		return (len);
+		return (_strlen_recursion(s + 1) + 1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
