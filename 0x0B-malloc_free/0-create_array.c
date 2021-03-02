93b78a4414280ff202840afa41ce54fb4a15fe02
Cristian Pinzón <faykris28@gmail.com>
2021-03-02 15:19:38 -0500

    update 0-create_array.c file fix betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,20 +13,21 @@
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i = 0;
-    char *arr;
-    if (size != 0)
-    {
-        arr = (char *) malloc(size * sizeof(int));
-        while (i < size)
-	    {
-	        arr[i] = c;
-	        i++;    
-	    }
-	    return (arr);
-    }
-    else
-    {
-        return (NULL);
-    }
+	unsigned int i = 0;
+	char *arr;
+
+	if (size != 0)
+	{
+		arr = (char *) malloc(size * sizeof(int));
+		while (i < size)
+		{
+			arr[i] = c;
+			i++;
+		}
+		return (arr);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
