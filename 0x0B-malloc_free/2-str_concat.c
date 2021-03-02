fd1016d445c88d7877682dd640d0395c9c081484
Cristian Pinzón <faykris28@gmail.com>
2021-03-02 18:43:47 -0500

    update 2-str_concat.c change \0 by NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,7 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k = 0, l = 0;
 	char *arr;
 
-	if (!s1)
-		return (NULL);
-	if (!s2)
+	if (!s1 || !s2)
 		return (NULL);
 	while (s1[i])
 		i++;
