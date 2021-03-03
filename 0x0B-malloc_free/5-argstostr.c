8b6732b7d59094c5e5537e69b760b8e050673776
Cristian Pinzón <faykris28@gmail.com>
2021-03-02 23:34:48 -0500

    update 5-argstostr.c fix j 2nd while

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * argstostr - function that concatenates all the arguments of your program
  * @ac: integer value
@@ -11,5 +11,41 @@
 
 char *argstostr(int ac, char **av)
 {
+	int i = 0, j = 0, k = 0, l = 0, len = 0;
+	char *p;
 
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	while (i < ac)
+	{
+		while (av[i][j] != '\0')
+		{
+			j++;
+		}
+		len += j + 1;
+		j = 0;
+		i++;
+	}
+	p = malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	len = 0;
+	while (k < ac)
+	{
+		while (av[k][l] != '\0')
+		{
+			*(p + len) = av[k][l];
+			len++;
+			l++;
+		}
+		*(p + len) = '\n';
+		l = 0;
+		len++;
+		k++;
+	}
+	return (p);
 }
