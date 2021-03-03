bbfc3383431e8146dcfc474ba2dd2a63ad8fd9e5
Cristian Pinzón <faykris28@gmail.com>
2021-03-02 22:03:40 -0500

    update 3-alloc_grid.c erase * in 2nd malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 	}
 	while (i < height)
 	{
-		p[i] = malloc(sizeof(int *) * width);
+		p[i] = malloc(sizeof(int) * width);
 		if (!p[i])
 		{
 			while (i >= 0)
