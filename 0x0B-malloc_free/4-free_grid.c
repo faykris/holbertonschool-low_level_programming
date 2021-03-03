bf97f79957bc12fd74f74750b2798ccec1e430c7
Cristian Pinzón <faykris28@gmail.com>
2021-03-02 22:41:38 -0500

    update 4-free_grid.c file

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by your alloc_grid function
+ * free_grid - frees a 2 dimensional grid previously created
+ *				by your alloc_grid function
  * @grid: pointer to poiter value
  * @height: int value
  *
@@ -11,5 +13,11 @@
 
 void free_grid(int **grid, int height)
 {
+	int i = 0;
 
+	while (i <= height)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
