a46b9428c7315e428971d566193e983af0c0e560
Cristian Pinzón <faykris28@gmail.com>
2021-03-04 13:34:41 -0500

    update 0-malloc_checked.c file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,11 +4,19 @@
 
 /**
  * malloc_checked - function that allocates memory using malloc
+ * @b: unsigned int value
  *
- * Return: Nothing
+ * Return: Nothing - 98 on error
  */
 
 void *malloc_checked(unsigned int b)
 {
+	void *ptr;
 
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
 }
