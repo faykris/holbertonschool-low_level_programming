8c27cca52df1e82f9a360ec31b6c43972a6774e0
Cristian Pinzón <faykris28@gmail.com>
2021-03-04 14:56:16 -0500

    update 1-string_nconcat.c add \0 add 1in malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	while (s2[j])
 		j++;
-	ptr = malloc(sizeof(char) * (i + j));
+	ptr = malloc(sizeof(char) * (i + j + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
