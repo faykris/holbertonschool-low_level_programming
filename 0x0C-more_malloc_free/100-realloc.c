4cfbb18b51c3338ff089e211cca981ac046a80bc
Cristian Pinzón <faykris28@gmail.com>
2021-03-07 12:53:12 -0500

    update/add .gitignore/100-realoc.c files

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,5 +13,32 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int i = 0;
+	char *newptr;
 
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < (new_size - old_size))
+	{
+		newptr[i] = *((char *)ptr + 1);
+		i++;
+	}
+	free(ptr);
+	return (newptr);
 }
