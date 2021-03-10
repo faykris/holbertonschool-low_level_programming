e99e2b16a2062332c3e309817b551ecdfb286614
faykris <faykris28@gmail.com>
2021-03-10 16:36:08 -0500

    update 0-print_name.c add all if conditions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,18 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - prints a name
  * @name: string pointer
  * @f: function pointer
+ * 
  * Return: nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
 	void (*print_name_as_is)(char *) = f;
 
-	if (!f)
+	if (!name && !f)
 	{
 		print_name_as_is(name);
 	}
