021e7bea271440fb1d96f8f7f20ab8f9930441f8
Cristian Pinzón <faykris28@gmail.com>
2021-03-09 19:07:36 -0500

    update .gitignore, 4-new_dog.c files

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,31 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+
+/**
+ * str_cpy - store the copy of values
+ * @str: string pointer
+¨* @len: int value
+ * Return: char value
+*/
+char *str_cpy(char *str, int len)
+{
+	char *new_str;
+	int i = 0;
+
+	new_str = malloc(len);
+	if (new_str == 0)
+	{
+		return (0);
+	}
+
+	while (i < len)
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	return (new_str);
+}
 
 /**
  * new_dog - function that creates a new dog
@@ -13,29 +37,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char* namcpy, owncpy;
-	dog_t *newdog; 
+	int i = 0, j = 0;
+	dog_t *newdog;
 
-	namcpy = strdup(name);
-	if (namcpy == NULL)
+	newdog = malloc(sizeof(struct dog));
+	if (newdog != 0 && name != 0 && owner != 0)
 	{
-		return (NULL);
+		while (name[i])
+		{
+			i++;
+		}
+		while (owner[j])
+
+		{
+			j++;
+		}
+		newdog->name = str_cpy(name, i + 1);
+		if (newdog->name == 0)
+		{
+			free(newdog);
+			return (0);
+		}
+		newdog->age = age;
+		newdog->owner = str_cpy(owner, j + 1);
+		if (newdog->owner == 0)
+		{
+			free(newdog->name);
+			free(newdog);
+			return (0);
+		}
+		return (newdog);
 	}
-	owncpy = strdup(owner);
-	if (owncpy == NULL)
-	{
-		free(namcpy);
-		return (NULL);
-	}
-	newdog = malloc(sizeof(dog_t));
-	if (newdog == NULL)
-	{
-		free(namcpy);
-		free(owncpy);
-		return (NULL);
-	}
-	newdog->name = namcpy;
-	newdog->age = age;
-	newdog->owner = owncpy;
-	return (newdog);
+	return (0);
+
 }
