2cc0302482d0cbb69bc4d758afb0f679fc6d4709
Cristian Pinzón <faykris28@gmail.com>
2021-03-26 19:59:00 -0500

    update 5-flip_bits.c fix betty and add free strings

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * print_binary - prints the binary representation of a number.
- * @n: string binary number.
+ * array_binary - return the binary representation of a number.
+ * @n: decimal number.
  *
- * Return: converted number, 0 when invalid character or b is NULL.
+ * Return: converted number to decimal in a string, NULL it's fails.
  */
 char *array_binary(unsigned long int n)
 {
@@ -33,31 +33,35 @@ char *array_binary(unsigned long int n)
 }
 
 /**
- * flit  - sets the value of a bit to 0 at a given index.
- * @n: number to analize in binary
- * @index: position of bit to return
+ * flip_bits- returns the number of bits you would need to flip to
+ *				get from one number to another.
+ * @n: decimal number to compare
+ * @m: decimal number to compare
  *
- * Return: value of the bit at index index or -1 if an error occured.
+ * Return: number of bits changed between m and n.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	char *bits1, *bits2;
-    int i = 0, j = 0;
-    
+	int i = 0, j = 0;
+
 	bits1 = array_binary(n);
 	bits2 = array_binary(m);
-	
-	if (bits1 == NULL || bits2 == NULL) 
+
+	if (bits1 == NULL || bits2 == NULL)
 	{
 		return (0);
 	}
 	while (bits1[i])
 	{
-	    if (bits1[i] != bits2[i])
-	    {
-	        j++;
-	    }
-	    i++;
+		if (bits1[i] != bits2[i])
+		{
+			j++;
+		}
+		i++;
 	}
+	free(bits1);
+	free(bits2);
+
 	return (j);
 }
