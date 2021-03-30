ce095cdaf063ba2cde3cff2e3e88d5fde99331c3
Cristian Pinzón <faykris28@gmail.com>
2021-03-29 21:18:58 -0500

    update 2-append_text_to_file.c add code test without if condition

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of file to create
+ * @text_content: tex contain for the file
+ *
+ * Return: 1 on success. On error -1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd = 0, i = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	while (text_content[i])
+	{
+		i++;
+	}
+	write(fd, text_content, i);
+	close(fd);
+
+	return (1);
+}
