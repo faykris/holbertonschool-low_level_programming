43abdad65f5c7ddc21b1f57c07aa1bfa7c83e698
Cristian Pinzón <faykris28@gmail.com>
2021-05-02 15:39:34 -0500

    modify task 7 - change logical

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,40 +10,36 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0, j = 0;
-	dlistint_t *tmp, *new;
+	dlistint_t *new, *tmp;
+	unsigned int i = 1;
 
-	if (idx == 0 || *h == NULL)
-		return (add_dnodeint(h, n));
-	tmp = *h;
-	while (tmp->next != NULL)
-	{
-		tmp = tmp->next;
-		i++;
-	}
-	if (idx > i + 1)
+	if (!h)
 		return (NULL);
-	if (idx == i + 1)
-		return (add_dnodeint_end(h, n));
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
 	new->n = n;
-	new->prev = NULL;
 	new->next = NULL;
-	if (*h == NULL)
-		return (new);
-	while (tmp->prev != NULL)
+	tmp = *h;
+
+	while (i < idx)
 	{
-		if (j == (i - idx))
-		{
-			new->prev = tmp->prev;
-			new->next = tmp;
-			tmp = new;
-			tmp->prev->next = tmp;
-		}
-		tmp = tmp->prev;
-		j++;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			break;
+		i++;
 	}
+	if (idx == 0)
+	{	new->next = tmp;
+		*h = new;
+	}
+	else if (tmp != NULL)
+	{
+		new->next = tmp->next;
+		tmp->next = new;
+	}
+	else
+		return (NULL);
+
 	return (new);
 }
