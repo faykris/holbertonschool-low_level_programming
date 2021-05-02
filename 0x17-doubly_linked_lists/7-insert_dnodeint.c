4f475c7b21578d9d08a25b3838ae4313b2dbc257
Cristian Pinzón <faykris28@gmail.com>
2021-05-02 18:46:35 -0500

    Modify task 7 add return (new) at last if

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,7 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			tmp = new;
 			tmp->prev->next = tmp;
 			tmp->next->prev = tmp;
-			break;
+			return (new);
 		}
 		tmp = tmp->prev;
 		j++;
