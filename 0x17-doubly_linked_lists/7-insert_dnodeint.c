75ccdf5eeab7ea5addbe018fd43b1aaa67f2c3f4
Cristian Pinzón <faykris28@gmail.com>
2021-05-02 19:18:30 -0500

    Modify task 7 test change *h conditionn

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0, j = 0;
 	dlistint_t *tmp, *new;
 
-	if (h == NULL || *h == NULL)
+	if (h == NULL)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
