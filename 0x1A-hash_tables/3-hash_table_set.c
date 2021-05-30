b83f0ae322408045ff638f0dd75729ad588bd9dc
Cristian Pinzón <faykris28@gmail.com>
2021-05-29 22:25:43 -0500

    fix 3-hast_table_set.c - add collision and ht = NULL conditions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,20 +13,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *item = NULL;
 	unsigned long int i;
 
+	if (ht == NULL)
+	{
+		return (0);
+	}
+
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
 	{
 		return (0);
 	}
+
 	item->key = calloc(strlen(key) + 1, 1);
 	item->value = calloc(strlen(value) + 1, 1);
 
 	i = key_index((const unsigned char *)key, ht->size);
 	strcpy(item->key, key);
 	strcpy(item->value, value);
-
+	if (ht->array[i] != NULL)
+	{
+		item->next = ht->array[i];
+	}
 	ht->array[i] = item;
 
 	return (1);
 }
-
