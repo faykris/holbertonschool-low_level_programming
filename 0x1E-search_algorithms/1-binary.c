f34c2d062ebd1c2db44d1c5a17aaeea44a39338d
faykris <faykris28@gmail.com>
2021-09-02 19:59:57 -0500

    update 1-binary.c - change recursive to iterative mode

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,43 +1,6 @@
 #include "search_algos.h"
 
 /**
- * recusive_search - Use Binary search algorithm with recursion
- * @array: array to be processed
- * @value: value to be searched in the array
- * @start: Index on the first position
- * @end: Index on the last position
- *
- * Return: Index of a element searched, -1 if not found
- */
-int recusive_search(int *array, int value, size_t start, size_t end)
-{
-	size_t pos = 0, i;
-
-	printf("Searching in array: ");
-	for (i = start; i <= end; i++)
-	{
-		if (i == start)
-			printf("%d", array[i]);
-		else
-		{
-			printf(", %d", array[i]);
-		}
-	}
-	printf("\n");
-
-	pos = (start + end) / 2;
-
-	if (array[pos] == value)
-		return (pos);
-	if (start == end)
-		return (-1);
-	if (array[pos] < value)
-		return (recusive_search(array, value, pos + 1, end));
-	else
-		return (recusive_search(array, value, 0, pos - 1));
-}
-
-/**
  * binary_search - Searches for a value in a sorted array of integers
  *					using the Binary search algorithm
  * @array: array to be processed
@@ -48,8 +11,31 @@ int recusive_search(int *array, int value, size_t start, size_t end)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
+	int lo = 0, mi = 0, hi = (int)size - 1, i;
+
+	if (array == NULL)
 		return (-1);
 
-	return (recusive_search(array, value, 0, size - 1));
+	while (hi >= lo)
+	{
+		mi = lo + (hi - lo) / 2;
+		printf("Searching in array: ");
+		for (i = lo; i <= hi; i++)
+		{
+			if (i == lo)
+				printf("%d", array[i]);
+			else
+			{
+				printf(", %d", array[i]);
+			}
+		}
+		printf("\n");
+		if (array[mi] == value)
+			return (mi);
+		if (array[mi] > value)
+			hi = mi - 1;
+		else
+			lo = mi + 1;
+	}
+	return (-1);
 }
